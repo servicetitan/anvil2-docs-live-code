ebccad20c7386b633831d5f5b847a192512cfdc9
Ryan De La Torre <rdelatorre@servicetitan.com>
2025-12-01 15:52:20 -0800

    Add examples and screenshots for Avatar image and name

---
+++ examples/avatar-image-and-name/src/App.tsx
@@ -0,0 +1,12 @@
+import { Avatar } from "@servicetitan/anvil2";
+
+function App() {
+  return (
+    <Avatar
+      image="https://anvil.servicetitan.com/Logo_block.jpg"
+      name="Titan"
+    />
+  );
+}
+
+export default App;
---
+++ examples/avatar-name-only/src/App.tsx
@@ -0,0 +1,12 @@
+import { Avatar, Flex } from "@servicetitan/anvil2";
+
+function App() {
+  return (
+    <Flex gap="2">
+      <Avatar name="Titan" />
+      <Avatar name="Service Titan" />
+    </Flex>
+  );
+}
+
+export default App;
