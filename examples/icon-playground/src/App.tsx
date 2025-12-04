62eb77b6dfc50d95a553861981512bc7c1557bcf
Ryan De La Torre <rdelatorre@servicetitan.com>
2025-12-03 16:20:07 -0800

    Add `Icon` example

---
+++ examples/icon-playground/src/App.tsx
@@ -0,0 +1,8 @@
+import { Icon } from "@servicetitan/anvil2";
+import Warning from "@servicetitan/anvil2/assets/icons/material/round/warning.svg";
+
+function App() {
+  return <Icon svg={Warning} size="large" />;
+}
+
+export default App;
