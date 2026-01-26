c306081a6dfffff91c9648ad7cd4202fe3cd3b93
anvil-automation <195246333+anvil-automation@users.noreply.github.com>
2026-01-26 18:07:47 +0000

    Version Packages

--- examples/button-size/src/App.tsx
+++ examples/button-size/src/App.tsx
@@ -3,10 +3,10 @@ import { Button, Grid } from "@servicetitan/anvil2";
 function App() {
   return (
     <Grid templateColumns="repeat(3, max-content)" gap="8" alignItems="center">
-      <Button size="large">Large</Button>
-      <Button>Medium</Button>
-      <Button size="small">Small</Button>
       <Button size="xsmall">Extra Small</Button>
+      <Button size="small">Small</Button>
+      <Button>Medium</Button>
+      <Button size="large">Large</Button>
     </Grid>
   );
 }
